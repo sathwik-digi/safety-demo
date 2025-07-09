d139071415055158939403c0948c5e9b07717d2f
siva sathwik <siva.majeti@digitalsprint.ai>
2025-07-09 13:00:05 +0530

    home page changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import notification from "../assets/Icons/notification-icon.png"
+import settings from "../assets/Icons/settings-icon.png"
 function Header() {
   return (
-    <div>Header</div>
+    <div className="flex p-3 gap-2 border-b-1 border-gray-200 items-center flex-row-reverse">
+      <img src={settings} className="h-5 w-5" />
+      <img src={notification} className="h-8 w-8" />
+    </div>
   );
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import HomeBgImage from "../assets/Images/Home-page-bg.jpg";
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
   const navigate = useNavigate();
   return (
-    <>
-      <div className="">HomePage</div>
-      <button className="border-solid border-2 p-4 my-8 bg-red-200 rounded-sm border-indigo-600" onClick={()=>navigate("/dashboard")}>Navigate to dashboard</button>
-    </>
+    <div className="h-screen w-screen bg-cover bg-center" style={{ backgroundImage: `url(${HomeBgImage})` }}>
+      <div className="grid grid-cols-2 justify-items-center gap-40 absolute top-120 left-[7vw]">
+        <h2 className="text-white w-140">
+          Andhra Pradesh Industrial Infrastructure Corporation Ltd.
+          (APIIC) was incorporated on 26th September, 1973 with
+          Authorised Capital of Rs.20.00 crores and paid up capital
+          of Rs.16.33 crores. APIIC is a wholly owned Undertaking of
+          Government of Andhra Pradesh.
+        </h2>
+        <div className="flex flex-col gap-4 ">
+          <Button className="bg-white w-70 rounded-[20px]" onClick={()=>navigate('/auth/login')}>Login</Button>
+          <Button className="bg-white w-70 rounded-[20px]" onClick={()=>navigate('/auth/register')}>Register</Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
